0e1c523a45c021ca31f4c086a1a873e4b8d50de0
Samuel Odecio <108842146+SamuelOdecio@users.noreply.github.com>
2022-10-24 19:40:26 -0300

    Resolução Exercicio02 udemyArray

--- udemyExerciciosArray/src/udemyexerciciosarray/Exercicio02.java
+++ udemyExerciciosArray/src/udemyexerciciosarray/Exercicio02.java
@@ -1,9 +1,31 @@
-
 package udemyexerciciosarray;
 
+import java.util.Scanner;
 
 public class Exercicio02 {
+
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int tamanho = entrada.nextInt();
+
+        int[] numeros = new int[tamanho];
+        int[] pares = new int[tamanho];
+        int qtdPares = 0;
+
+        for (int i = 0; i < tamanho; i++) {
+            numeros[i] = entrada.nextInt();
+            
+            if (numeros[i] % 2 == 0) {
+                pares[i] = numeros[i];
+                qtdPares++;
+            }
+        }
+        System.out.println("NUMEROS PARES: ");
+        for (int i = 0; i < qtdPares; i++) {
+
+            System.out.print(pares[i] + " ");
+        }
         
+        System.out.println("\nQUANTIDADE DE PARES: "+ qtdPares);
     }
 }
